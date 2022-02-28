7baa45b8fff9b6c0c6e3c5343e11c77d34a76eaf
emicludo <86956780+emicludo@users.noreply.github.com>
2022-02-28 17:07:42 +0100

    creating-product-list

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function render(data) {
         newCard.classList.add('col')
          // is a node
         newCard.innerHTML = `<div class="card">
-                <img src="`+ element.image +`" class="card-img-top img-fluid"  style="max-height: 300px" alt="...">
+                <div><img src="`+ element.image +`" class="card-img-top p-4" width=100 height=100 alt="..."></div>
                 <div class="card-body">
                     <h5 class="card-title">`+ element.title +`</h5>
                     <p class="card-text">`+ element.type +`</p>
                     <a href="#" class="btn btn-primary">See details</a>
                 </div>
             </div>`
-        document.getElementById("allProducts").appendChild(newCard)
+        document.getElementById("offerProducts").appendChild(newCard)
     });
 }
