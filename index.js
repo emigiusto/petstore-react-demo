282bd8c60d8217efd31ca1fed1c15296d287c7ca
emicludo <86956780+emicludo@users.noreply.github.com>
2022-02-28 16:11:42 +0100

    index-working-on-cardDeck

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ function addEventListeners() {
 
 function render(data) {
     data.forEach(element => {
-        var newCard = document.createElement('li'); // is a node
-        newCard.innerHTML = `<div class="card" style="width: 18rem;">
-                <img src="`+ element.image +`" class="card-img-top" alt="..." width=150 height=auto>
+        var newCard = document.createElement('div');
+        newCard.classList.add('col')
+         // is a node
+        newCard.innerHTML = `<div class="card">
+                <img src="`+ element.image +`" class="card-img-top img-fluid"  style="max-height: 300px" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">`+ element.title +`</h5>
                     <p class="card-text">`+ element.type +`</p>
