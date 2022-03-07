2da21c8191fbf3d87e6c2e85ae8763ef675c9987
Alexander Hemmingsen <alexhemmingsen1@gmail.com>
2022-03-07 19:19:15 +0100

    Clear cart functionality in cart.html

--- cart/cart.js
+++ cart/cart.js
@@ -1,40 +1,60 @@
+import {clearCart} from "/helperFunctions/cart.js"
 
 window.onload = function() {
     loadProduct();
     addEventListeners()
 };
-var data;
 function loadProduct() {
     var MyDataString = localStorage.getItem("product-array")
     var data = JSON.parse(MyDataString)
     render(data)
 }
 
+function handleClearCart() {
+    clearCart()
+    loadProduct()
+}
+
+function addEventListeners() {
+    document.getElementById("clearCartButton").addEventListener("click", function() {
+        handleClearCart()
+    })
+}
+
 function render(data) {
-    data.forEach(element => { 
-        var newProduct = document.createElement('div');
-        newProduct.classList.add('col-sm-4')
-         // is a node
-        newProduct.innerHTML = `<li class="row">
-        <div class="col-lg-3 product-img-layout">
-            <img class= "product_img" src="`+ element.image +`" alt="">
-        </div>
-        <div class="col product-text-container">
-            <div class="row">
-                <div class="col">
-                    <p>`+ element.title +`</p>
+    document.getElementById("cartList").innerHTML = ""
+    if (data) {
+        data.forEach(element => { 
+            var newProduct = document.createElement('div');
+            newProduct.classList.add('col-sm-4-auto')
+             // is a node
+            newProduct.innerHTML = `<li class="row">
+            <div class="col-lg-3 product-img-layout">
+                <img class= "product_img" src="`+ element.image +`" alt="">
+            </div>
+            <div class="col product-text-container">
+                <div class="row">
+                    <div class="col">
+                        <p>`+ element.title +`</p>
+                    </div>
+                    <div class="col product-price-container">
+                        <p class= "product-text-price">`+ element.price +`</p>
+                    </div>
                 </div>
-                <div class="col product-price-container">
-                    <p class= "product-text-price">`+ element.price +`</p>
+                <div class="row">
+                    <div class="col">
+                        <p class="product-text-subtext">`+ element.description +`</p>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <p class="product-text-subtext">`+ element.description +`</p>
-                </div>
-            </div>
-        </div>
-    </li>`
+        </li>`
         document.getElementById("cartList").appendChild(newProduct)
-    });
+        });
+    } else {
+        var newProduct = document.createElement('div');
+        newProduct.classList.add('col-sm-4.auto')
+        newProduct.innerHTML = '<p class="text-center" style="padding-top: 24px;">Your cart is empty</p>'
+        document.getElementById("cartList").appendChild(newProduct)
+    }
+    
 }
