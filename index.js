a130a786c06b449d9a9a62fe1f5b876af9c55028
Finn Baumann <hello@baumannfinn.de>
2022-03-07 17:29:20 +0100

    Changed header into a module and included it in index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {createFooter} from "./components/footer.js"
+import {createHeader} from "./components/header.js"
 
 window.onload = function() {
     loadProducts();
     addEventListeners()
     renderFooter();
+    renderHeader();
 };
 var data;
 function loadProducts() {
@@ -19,6 +21,10 @@ function addEventListeners() {
 
 function renderFooter(){
     document.getElementById("footer").innerHTML = createFooter();
+}
+
+function renderHeader(){
+    document.getElementById("header").innerHTML = createHeader();
 }
 
 function render(data) {
