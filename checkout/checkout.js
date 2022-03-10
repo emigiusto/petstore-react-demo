05b47d4ba65b99b595abb18f0d93c5c933280916
Nicholas Gioachini <ngio@itu.dk>
2022-03-10 13:45:08 +0100

    connecting as in wireframe + addons

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,19 +1,3 @@
-import {createFooter} from "/components/footer.js"
-import {createHeader} from "/components/header.js"
-
-window.onload = function() {
-    renderHeader();
-    renderFooter();
-};
-
-function renderFooter(){
-    document.getElementById("footer").innerHTML = createFooter();
-}
-
-function renderHeader(){
-    document.getElementById("navbar").innerHTML = createHeader();
-}
-
 
 function saveForm(){
     saveNames();
