b1e42d3804f43f145adf02b9de757852a647dbb4
emicludo <86956780+emicludo@users.noreply.github.com>
2022-04-08 12:23:04 +0200

    minor corrections to front page

--- api/app.js
+++ api/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-/* var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users'); */
+var indexRouter = require('./routes/index');
 var productRouter = require('./routes/products/products.route.js');
 var usersRouter = require('./routes/users/users.route.js');
 var orderRouter = require('./routes/orders/orders.route');
@@ -18,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use('/', indexRouter); */
+app.use('/', indexRouter);
 app.use('/users', usersRouter); 
 app.use('/products', productRouter);
 app.use('/orders', orderRouter)
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,29 +1,10 @@
 
 var express = require('express');
 var router = express.Router();
-//var { collection, getDocs } = require('firebase/firestore');
-var db = require("../dba/firestore")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  //res.render('index', { title: 'Express' });
-  //log().then(response => console.log(response))
-  getAllProducts().then(response =>  res.send(response) )
+  res.send("Welcome to Pet Store");
 });
-
-const getAllProducts = async () => {
-  const products = []
-    const productColletion = db.collection('products');
-    const snapshot = await productColletion.get();
-    snapshot.forEach(doc => {
-      products.push({
-        ...doc.data(),
-        id: doc.id
-      });
-    });
-    return products
-}
-
-
 
 module.exports = router;
