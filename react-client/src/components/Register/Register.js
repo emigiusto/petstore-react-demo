980f4488737e3149f525a44e16ac35274ecc4d4c
Finn Baumann <hello@baumannfinn.de>
2022-05-08 19:50:57 +0200

    cleanup of error messages

--- react-client/src/components/Register/Register.js
+++ react-client/src/components/Register/Register.js
@@ -1,8 +1,6 @@
 //Currently unused, see RegisterForm instead
 
 import React from "react";
-import Backdrop from "../GeneralComponents/Backdrop/Backdrop";
-import RegisterForm from "./RegisterForm/RegisterForm";
 
 function Register() {
    return <div></div>
