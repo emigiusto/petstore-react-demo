452385f596bd47aafa98f9fa3726d83388dd92c5
Fernando <fers@itu.dk>
2022-05-23 17:13:57 +0200

    ordercompleted without variables

--- react-client/src/components/Checkout/orderCompleted.js
+++ react-client/src/components/Checkout/orderCompleted.js
@@ -9,29 +9,30 @@ export default function orderCompleted() {
   return (
     <div>
       <Header />
-      <ProductConsumer>
         
+      <ProductConsumer>
         {(context) => {
           return(
-            
-      <div className="row justify-content-center p-4">
-
-        <h3> Thanks for Shopping with Us!</h3>
-        <h2> You will receive an email at
-          { (context.user.email != null) ? (' ' + context.user.email + ' ') : " your email " } 
-          once we have processed your order</h2>
+            <div>     
+          <div className="row justify-content-center p-4">
           <div className="w-50 p-3 row justify-content-center">
+          <h1> Thanks for Shopping with Us!</h1>
+          <h2> You will receive the details in
+            {/* a call to context.user.email would look better*/}
+          { true ? (' ' + "your email" + ' ') : " your email " } 
+          once we have processed your order</h2>
             <img
                 className="rounded mx-auto d-block"
                 src='https://animatedkid.files.wordpress.com/2013/08/madagascar15.png'
                 alt="" />
           </div>
           </div>
+        </div>
           );
         }}
       </ProductConsumer>
       <Footer />
-    </div>
+      </div>
     );
   }
   
