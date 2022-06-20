0982cacde3b87a20583b6cffbd4959555ea1658d
Alexander Hemmingsen <alexhemmingsen1@gmail.com>
2022-06-20 16:08:38 +0200

    Fixed goback button

--- react-client/src/components/GeneralComponents/GoBackButton.js
+++ react-client/src/components/GeneralComponents/GoBackButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../Checkout/Checkout.css'
 
 export default function GoBackButton() {
 
@@ -9,7 +10,7 @@ export default function GoBackButton() {
 
   return (
     <div className="col-sm-12 d-flex justify-content-left px-5 mx-5">
-        <button className="goback px-3" onClick = {() => navigate(-1)}> <FontAwesomeIcon icon={faArrowLeft} />Go back</button> 
+        <div className="goback px-3" onClick = {() => navigate(-1)}> <FontAwesomeIcon icon={faArrowLeft} />Go back</div> 
     </div>
   )
 }
